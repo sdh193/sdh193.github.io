c109225437c311ebaa71f2a98404d42b685bfed7
sdh193 <84048042+sdh193@users.noreply.github.com>
2021-05-13 15:40:25 -0700

    Update Javascript.js

--- secure/math_language/Javascript.js
+++ secure/math_language/Javascript.js
@@ -2,4 +2,4 @@ var a = localStorage.getItem('password')
 var b = localStorage.getItem('captchaa')
 var d=new Date()
 if (a>2051 && a<2053 && b>(((d.getHours()*8992301.61)+(d.getDay()*1023198.79)-1) && b<((d.getHours()*8992301.61)+(d.getDay()*1023198.79)+1))) {
-}else{window.location = "file:///C:/Users/start_dhzj9kj/OneDrive/Desktop/Benjamin%20Code%20Files/Text%20and%20test%20files/html%20documents/Benjamin's%20math%20language.html"}
+}else{window.location = "https://sdh193.github.io/secure/math_language/"}

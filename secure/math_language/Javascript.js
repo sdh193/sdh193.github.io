d03b078c9d059efb01a8011e5a71c3cf76207b97
sdh193 <84048042+sdh193@users.noreply.github.com>
2021-05-13 16:17:38 -0700

    Update Javascript.js

--- secure/math_language/Javascript.js
+++ secure/math_language/Javascript.js
@@ -2,4 +2,4 @@ var a = localStorage.getItem('password')
 var b = localStorage.getItem('captchaa')
 var d=new Date()
 if (a>2051 && a<2053 && b>(((d.getHours()*8992301.61)+(d.getDay()*1023198.79)-1) && b<((d.getHours()*8992301.61)+(d.getDay()*1023198.79)+1))) {
-}else{window.location = "/"}
+}else{window.location = "/secure/math_language"}

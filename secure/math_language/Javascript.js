832de784d47add252cf348e6f27d701337056783
sdh193 <84048042+sdh193@users.noreply.github.com>
2021-05-14 10:17:30 -0700

    Update Javascript.js

--- secure/math_language/Javascript.js
+++ secure/math_language/Javascript.js
@@ -1,3 +1,6 @@
+var a = localStorage.getItem('password') 
+var b = localStorage.getItem('captchaa')
+var d=new Date()
 addEventListener('contextmenu', function(e){e.preventDefault(); document.onkeydown = function(e) {if(event.keyCode == 123) {return false;};if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))return false;};if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {return false;}if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {return false;}if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {return false;}if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {return false;};}); 
 if (a>2051 && a<2053 && b>((d.getMinutes()*39103.18)+(d.getHours()*8992301.61)+(d.getDay()*1023198.79)-1) && b<((d.getMinutes()*39103.18)+(d.getHours()*8992301.61)+(d.getDay()*1023198.79)+1)) {
-}else{window.location = "sdh193.github.io/secure/math_language"}
+}else{window.location = "index.html"}
